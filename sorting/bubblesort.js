ccd382c137a36f3f4b0386d08dc8cf7e44a7e0bc
nikkig124 <nikkig124@gmail.com>
2022-01-30 09:59:49 -0800

    bubble sort

---
+++ sorting/bubblesort.js
@@ -0,0 +1,17 @@
+let arr = [123, 42, 12, 9, 400, 23, 1, 0, 21];
+
+function bubbleSort(arr) {
+    for (i = 0; i < arr.length; i++) {
+        for (j = arr.length - 1; j > i; j--) {
+            console.log(arr[j], arr[j-1])
+            if (arr[j-1] > arr[j]) {
+                let temp = arr[j];
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arr));
