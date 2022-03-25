dfce9f10cf4b1592227769618a4369cf72c524b0
nikkig124 <nikkig124@gmail.com>
2022-03-24 19:44:55 -0700

    add bool return solution

--- recursion/practice/target.py
+++ recursion/practice/target.py
@@ -1,3 +1,35 @@
+def check_if_sum_possible_bool(arr, k):
+    """
+    Args:
+     arr(list_int64)
+     k(int64)
+    Returns:
+     bool
+    """
+    # Write your code here.
+
+    return helper(0, 0, 0, arr, k)
+
+def helper(i, count, sum, arr, k):
+
+    if sum == k:
+        if count != 0:
+            return True
+
+    if i == len(arr):
+        return False
+
+    left = helper(i+1, count+1, sum+arr[i], arr, k)
+
+    if left:
+        return left
+    right = helper(i+1, count, sum, arr, k)
+
+    return right
+
+print(check_if_sum_possible_bool([1, 2, 3], 6))
+
+
 def check_if_sum_possible(arr, k):
     """
     Args:
@@ -9,17 +41,10 @@ def check_if_sum_possible(arr, k):
     # Write your code here.
     result = []
     helper(0, [], arr, k, result)
-    print(result)
-    if result:
-        return True
-    return False
+    return result
 
 def helper(i, partial, arr, k, result):
 
-    # if i >= len(arr):
-    #     if sum(partial) == k:
-    #         count+=1
-    #     return
     if partial and sum(partial) == k:
 
         result.append(partial)
@@ -33,33 +58,3 @@ def helper(i, partial, arr, k, result):
     partial.append(arr[i])
     helper(i+1, partial, arr, k, result)
     partial.pop()
-
-# def check_if_sum_possible(arr, k):
-#     """
-#     Args:
-#      arr(list_int64)
-#      k(int64)
-#     Returns:
-#      bool
-#     """
-#     # Write your code here.
-#     possible = 0
-#     helper(0, [], arr, k, possible)
-#     return possible
-
-# def helper(i, partial, arr, k, possible):
-#     print(partial, sum(partial))
-#     if sum(partial) == k:
-#         possible = 1
-#         return
-
-#     if i >= len(arr):
-#         return
-
-#     helper(i+1, partial, arr, k, possible)
-
-#     partial.append(arr[i])
-#     helper(i+1, partial, arr, k, possible)
-#     partial.pop()
-
-print(check_if_sum_possible([10, 20], 0))
